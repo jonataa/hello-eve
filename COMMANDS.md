812eb3b348a9298d188e8e286cbca62a9234b5a8
Jonata Weber <jonataa@gmail.com>
2017-08-05 03:08:31 -0300

    add step 4 example

--- COMMANDS.md
+++
@@ -1,3 +0,0 @@
-http localhost:5000/emails
-http POST localhost:5000/emails author=5985513dd8004489a5393c7f subject='Hello World' body='This is a hello world'
-http localhost:5000/emails embedded=='{"author": 1}'
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from eve import Eve
 
+def add_people_fullname(response):
+    for item in response['_items']:
+        item['fullname'] = item['firstname'] + " " + item['lastname']
+
 app = Eve()
+app.on_fetched_resource_people += add_people_fullname
 app.run(debug=True)
