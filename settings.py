d70b49c4accd28b1fdc1b6c357f06713baf0699d
Jonata Weber <jonataa@gmail.com>
2017-08-05 01:16:35 -0300

    add step 1 example

--- settings.py
+++ settings.py
@@ -1,1 +1,47 @@
-DOMAIN = {'people': {}}
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/pyeve/cerberus) for details.
+    'firstname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 10,
+    },
+    'lastname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15,
+        'required': True,
+        # talk about hard constraints! For the purpose of the demo
+        # 'lastname' is an API entry-point, so we need it to be unique.
+        'unique': True,
+    },
+    # 'role' is a list, and can only contain values from 'allowed'.
+    'role': {
+        'type': 'list',
+        'allowed': ["author", "contributor", "copy"],
+    },
+    # An embedded 'strongly-typed' dictionary.
+    'location': {
+        'type': 'dict',
+        'schema': {
+            'address': {'type': 'string'},
+            'city': {'type': 'string'}
+        },
+    },
+    'born': {
+        'type': 'datetime',
+    },
+}
+
+people = {'schema': schema}
+
+DOMAIN = {'people': people}
